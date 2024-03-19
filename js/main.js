cc34431da0ed1ae9cfa7461c4f79b78673f92963
diana <dskripka005@gmail.com>
2024-03-19 20:20:59 +0200

    fixtaskone

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Завдання 1
+let count = 0;
+const interval = setInterval(() => {
+    count++;
+    console.log("Повідомлення номер " + count);
+    if (count === 5) {
+        clearInterval(interval);
+    }
+}, 1000);
+
+//Завдання 2
+
+
+//Завдання 3
+
+
+//Завдання 4
